5e3981003c4aae216bd1b587eaeb395732a57707
Lewis Blackburn <lewisblackburn10@gmail.com>
2021-03-21 13:56:14 +0000

    removed handleRedirect

--- kibbeh/src/lib/handleRedirect.ts
+++
@@ -1,8 +0,0 @@
-export const handleRedirect = (router: any) => {
-  if (typeof router.query.next === "string") {
-    router.push(router.query.next);
-  } else {
-    // worked
-    router.push("/");
-  }
-};
